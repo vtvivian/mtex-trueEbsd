22a115b5e64b2e80567a23bcce991888bb4d45ff
Vivian <vtvivian@users.noreply.github.com>
2026-01-13 14:02:14 +0000

    export_fig needs char inputs

--- trueEbsd/tools/saveFigs.m
+++ trueEbsd/tools/saveFigs.m
@@ -5,12 +5,13 @@ function [] = saveFigs(figHandle,imName,pName,varargin)
 % enable optional inputs (varargin) into export_fig
 % 2020-08-25
 % set white figure background
+% export_fig can't handle strings properly, convert saveLoc to char array
 
 if ~isfolder(pName)
    mkdir(pName);
 end
 
-saveLoc = fullfile(pName,imName);
+saveLoc = char(fullfile(pName,imName));
 
 try figHandle.Color = 'w'; end %sometimes doesn't work with mtex figures, not dealbreaker
 
